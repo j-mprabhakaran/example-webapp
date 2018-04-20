1058003e2e54ad4455dfb148eea529449762a755
Prabhakaran Jayaraman Masani <31499990+jmprabhakaran@users.noreply.github.com>
2018-04-20 12:07:18 +0530

    Update run-stack.sh

--- run-stack.sh
+++ run-stack.sh
@@ -9,4 +9,4 @@ aws cloudformation $ACTION \
     ParameterKey=DockerImage,ParameterValue=981073932957.dkr.ecr.ap-south-1.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
     ParameterKey=VPC,ParameterValue=vpc-716e1a18 \
     ParameterKey=Cluster,ParameterValue=default \
-    ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:ap-south-1:981073932957:listener/app/production-website/72aef657c260904c/ffc65e7282c46ccd
+    ParameterKey=Listener,ParameterValue=$ALB_LISTENER_ARN
